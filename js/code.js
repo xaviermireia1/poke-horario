09123fee8bff37a82af8b10b9e1a234c9fac2e5a
Xavi <100006207.joan23@fje.edu>
2021-11-19 19:37:18 +0100

    Ejercicio 5

--- js/code.js
+++ js/code.js
@@ -32,10 +32,25 @@ function getListRandom(n, min, max) {
     return array;
 }
 
+function listRandom() {
+    buttonOpacity();
+    galleryRandom(0, 10, null, null);
+}
+
 function buttonOpacity() {
     if (document.getElementById("pokeball").style.opacity == "1") {
         document.getElementById("pokeball").style.opacity = "0.5";
     } else {
         document.getElementById("pokeball").style.opacity = "1";
     }
+}
+
+function galleryRandom(min, max, images, listRnd) {
+    images = document.getElementsByTagName("img");
+    listRnd = getListRandom(10, 1, 10);
+    for (let i = min; i < max; i++) {
+        images[i].setAttribute("src", "./img/IMG_" + listRnd[i] + ".PNG");
+        console.log(images[i]);
+    }
+    return images;
 }
